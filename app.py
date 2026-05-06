5d665c6279d71e96456d2c5e724f3771bb7f1c3e
SMITAGADE <163650927+SMITAGADE@users.noreply.github.com>
2026-05-06 14:55:31 +0530

    Change homepage message from 'hello world' to 'Hello Cloud'

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
+# Create Flask application object
 app = Flask(__name__)
+
+# Define route (homepage)
 @app.route("/")
 def home():
-    return "hello world"
+    return "Hello Cloud"
+
+# Run the app
 if __name__ == "__main__":
     app.run()
